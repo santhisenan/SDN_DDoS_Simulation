650dc7fd137e93ea8acb430e064531b5db79f609
Vishnu Kaimal <kaimal97@gmail.com>
2019-03-04 10:49:04 +0530

    Randomize selection of hosts in mininet script:

--- tree_topology.py
+++ tree_topology.py
@@ -2,15 +2,24 @@ from mininet.net import Mininet
 from mininet.topolib import TreeTopo
 from mininet.node import Controller, OVSKernelSwitch, RemoteController
 import time
+import random
 
 tree_topo = TreeTopo(depth=2, fanout=2)
 net = Mininet(topo=tree_topo, controller=RemoteController)
 net.start()
 
-# for i in range(20):
-# time.sleep(10)
-# net.pingAll()
-h1 = net.hosts[0]
-result = h1.cmd('hping3 -1 --flood  10.0.0.4')
-print(result)
-# net.stop()
+episode_count = 100
+episode_length = 10
+no_of_hosts = 4
+victim_host_ip = '10.0.0.' + str(no_of_hosts)
+spoofed_ip = '10.1.1.1'
+
+for i in range(episode_count):
+    rand_host = random.randint(0, no_of_hosts - 1) # select a random host in between 1 and no_of_hosts - 1
+    host = net.hosts[rand_host]
+    # Attack the last host with IP 10.0.0.4
+    # timout command is used to abort the hping3 command after the attack was performed for the specifed time
+    host.cmd('timeout ' + str(episode_length) + 's hping3 -1 --flood -a '+ spoofed_ip +' '+ victim_host_ip) 
+
+
+net.stop()
