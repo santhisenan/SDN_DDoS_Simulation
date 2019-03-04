f05a9682ccccc93fd241657967712acca3caecbf
Vishnu Kaimal <kaimal97@gmail.com>
2019-03-04 11:29:44 +0530

    Add threads to send benign packets to victim host in mininet script

--- tree_topology.py
+++ tree_topology.py
@@ -3,6 +3,8 @@ from mininet.topolib import TreeTopo
 from mininet.node import Controller, OVSKernelSwitch, RemoteController
 import time
 import random
+# from random import choice
+import threading
 
 tree_topo = TreeTopo(depth=2, fanout=2)
 net = Mininet(topo=tree_topo, controller=RemoteController)
@@ -14,12 +16,25 @@ no_of_hosts = 4
 victim_host_ip = '10.0.0.' + str(no_of_hosts)
 spoofed_ip = '10.1.1.1'
 
-for i in range(episode_count):
-    rand_host = random.randint(0, no_of_hosts - 1) # select a random host in between 1 and no_of_hosts - 1
-    host = net.hosts[rand_host]
+def ddos_flood(host):
     # Attack the last host with IP 10.0.0.4
     # timout command is used to abort the hping3 command after the attack was performed for the specifed time
-    host.cmd('timeout ' + str(episode_length) + 's hping3 -1 --flood -a '+ spoofed_ip +' '+ victim_host_ip) 
+    host.cmd('timeout ' + str(episode_length) + 's hping3 -1 --flood -a '+ spoofed_ip +' '+ victim_host_ip)
 
+def ddos_benign(host):
+    host.cmd('timeout ' + str(episode_length) + 's hping3 -1 --fast '+ victim_host_ip)
+   
+for i in range(episode_count):
+    attacking_host_id = random.randint(0, no_of_hosts - 1) # select a random host in between 1 and no_of_hosts - 1
+    attacking_host = net.hosts[attacking_host_id]
+
+    benign_host_id = random.choice([i for i in range(0, no_of_hosts - 1) if i not in [attacking_host_id]])
+    benign_host = net.hosts[benign_host_id]
+
+    t1 = threading.Thread(target=ddos_benign, args=(benign_host,)) 
+    t1.start()
+    ddos_flood(attacking_host)
+
+    t1.join()
 
 net.stop()
